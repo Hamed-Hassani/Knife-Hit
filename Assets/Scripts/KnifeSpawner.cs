57dfc7c0b8cf9c8b061c4dfb858a9b88e66da3ff
HamedHassani <hamed2008ir@gmail.com>
2022-05-11 10:41:54 +0430

    upload game 2D

---
+++ Assets/Scripts/KnifeSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KnifeSpawner : MonoBehaviour
+{
+
+    public Transform spawnPoint;
+    public GameObject KnifePrefab;
+
+    private Text scoreText;
+    private int score;
+
+    private void Awake()
+    {
+        scoreText = GameObject.Find("Score Text").GetComponent<Text>();
+    }
+
+    public void spawnKife()
+    {
+        GameObject go = Instantiate(KnifePrefab, spawnPoint.position, spawnPoint.rotation);
+        go.transform.parent = spawnPoint;
+
+    }
+    public void IncrecmentScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+}
